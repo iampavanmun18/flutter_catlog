473053c360b637a4583baed2d449e46b012aa2e1
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-04-27 12:23:14 +0530

    added Navigate back to Login Screen using Navigation.pop

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_catlog/pages/login_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,18 +11,27 @@ class HomePage extends StatelessWidget {
     int days = 30;
     String name = "Pavan";
     return Scaffold(
-      // themeMode: ThemeMode.dark, // add dark theme
-      // theme: ThemeData(primarySwatch: Colors.blue),
-      // darkTheme: ThemeData(brightness: Brightness.dark),
-      // debugShowCheckedModeBanner: false,
-      appBar: AppBar(
-        title: Text("Catlog App"),
-      ),
-      body: Center(
-        child:
-            Container(child: Text("Welcome to $days days of flutter by $name")),
-      ),
-      drawer: Drawer(),
-    );
+        // themeMode: ThemeMode.dark, // add dark theme
+        // theme: ThemeData(primarySwatch: Colors.blue),
+        // darkTheme: ThemeData(brightness: Brightness.dark),
+        // debugShowCheckedModeBanner: false,
+        appBar: AppBar(
+          title: Text("Catlog App"),
+        ),
+        body: Center(
+          child: Column(children: [
+            Container(
+              child: Text("Welcome to $days days of flutter by $name"),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()));
+                },
+                child: Text("Back Screen"))
+          ]),
+        ));
   }
 }
