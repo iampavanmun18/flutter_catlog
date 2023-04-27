63e1770d78ea105c59d31051c3692892845c039e
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-04-27 12:24:46 +0530

    added Animation,New layout for login screen and added validation for TextFormFields

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,52 +12,132 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  bool changeButton = false;
+  final _formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("../../assets/images/login_image.png",
-              fit: BoxFit.contain),
-          const SizedBox(height: 20),
-          Text(
-            "Welcome $name",
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
-            child: Column(
-              children: [
-                TextFormField(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // color: Colors.white,
+      appBar: AppBar(
+        title: const Center(child: Text("Login Page")),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Image.asset("../../assets/images/login_image2.png",
+                  fit: BoxFit.contain),
+              const SizedBox(height: 20),
+              Text(
+                "Welcome $name",
+                style:
+                    const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              // Padding(
+              //   padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 16),
+              //   child: Column(
+              //     children: [
+              //       TextFormField(
+              //         decoration: const InputDecoration(
+              //             hintText: "Enter Username", labelText: "Username"),
+              //         onChanged: (value) {
+              //           name = value;
+              //           setState(() {});
+              //         },
+              //       ),
+              //       TextFormField(
+              //         obscureText: true,
+              //         decoration: const InputDecoration(
+              //             hintText: "Enter Password", labelText: "Password"),
+              //       ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
                   decoration: const InputDecoration(
-                      hintText: "Enter Username", labelText: "Username"),
-                  onChanged: (value) {
-                    name = value;
-                    setState(() {});
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                      hintText: 'Enter UserName'),
+                  // onChanged: (value) => {name = value, setState(() => {})},
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      RequestFocusAction();
+                      return "UserName cannot be empty";
+                    }
+                    return null;
                   },
                 ),
-                TextFormField(
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
                   obscureText: true,
                   decoration: const InputDecoration(
-                      hintText: "Enter Password", labelText: "Password"),
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      hintText: 'Enter Password'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Password cannot be empty";
+                    } else if (value.length <= 6) {
+                      return "Password length should be greater than 6";
+                    }
+                    return null;
+                  },
+                  // onChanged: (value) => {name = value, setState(() => {})},
                 ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const HomePage()));
-                    },
-                    style: TextButton.styleFrom(),
-                    child: Text("Login"))
-              ],
-            ),
-          )
-        ],
+              ),
+              const SizedBox(height: 20),
+              Material(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(changeButton ? 50 : 8),
+                child: InkWell(
+                  // onTap: () async {
+                  //   await Future.delayed(Duration(seconds: 3));
+                  //   // ignore: use_build_context_synchronously
+                  //   Navigator.push(
+                  //       context,
+                  //       MaterialPageRoute(
+                  //           builder: (context) => const HomePage()));
+                  // },
+                  child: AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    width: changeButton ? 50 : 150,
+                    height: 50,
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          moveToHome(context);
+                        },
+                        style: TextButton.styleFrom(),
+                        child: changeButton
+                            ? const Icon(Icons.done)
+                            : const Text("Login")),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  Future<void> moveToHome(BuildContext context) async {
+    if (_formkey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+
+      // ignore: use_build_context_synchronously
+      await Navigator.push(
+          context, MaterialPageRoute(builder: (context) => const HomePage()));
+      setState(() {
+        changeButton = false;
+      });
+    }
   }
 }
