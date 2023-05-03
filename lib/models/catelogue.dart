a3ef4fa25d0886f6992e35555250d9ea3aafc4b4
Pavan Mundada <pavanpmundada5793@gmail.com>
2023-05-03 16:49:34 +0530

    added getById and getByPosition static methods statically to access them inside Catlog_list

--- lib/models/catelogue.dart
+++ lib/models/catelogue.dart
@@ -1,5 +1,13 @@
 class CatelogModel {
   static List<Item>? items = null;
+
+//get item by Id
+  static Item getById(int id) =>
+      items!.firstWhere((element) => element.id == id,
+          orElse: null); //to get element by matching  selected element with id
+
+//get item by position
+  static Item getByPosition(int pos) => items![pos];
 }
 
 class Item {
@@ -19,7 +27,7 @@ class Item {
       required this.image});
 
   factory Item.fromMap(Map<String, dynamic> map) {
-   return Item(
+    return Item(
       id: map["id"],
       name: map["name"],
       desc: map["desc"],
